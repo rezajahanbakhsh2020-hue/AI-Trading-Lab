381afd70f971b95f65ac7df439086691629c936c
rezajahanbakhsh2020-hue <reza.jahanbakhsh2020@gmail.com>
2026-09-04 12:42:25 +0330

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,1 +1,15 @@
+from pathlib import Path
 
+
+# Project root
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+# Main directories
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+FEATURES_DATA_DIR = DATA_DIR / "features"
+
+RESULTS_DIR = PROJECT_ROOT / "results"
+CONFIGS_DIR = PROJECT_ROOT / "configs"
+NOTEBOOKS_DIR = PROJECT_ROOT / "notebooks"
